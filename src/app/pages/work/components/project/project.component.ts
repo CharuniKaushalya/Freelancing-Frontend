995598d1d56c98f887eb3ff16d34eb113ed40427
KanchanaMalsha <malshathilini10.13@cse.mrt.ac.lk>
2017-08-24 21:20:51 +0530

    edited project UIs

--- src/app/pages/work/components/project/project.component.ts
+++ src/app/pages/work/components/project/project.component.ts
@@ -1,40 +1,41 @@
-import {Component} from '@angular/core';
-import { MyService } from  "../../../../theme/services/backend/service";
-import {TreeModel} from 'ng2-tree';
-import { Router} from '@angular/router';
+import { Component } from '@angular/core';
+import { MyService } from "../../../../theme/services/backend/service";
+import { TreeModel } from 'ng2-tree';
+import { Router } from '@angular/router';
 
 import { Project } from "../../../../theme/models/project";
 
 @Component({
-  selector: 'my-projects',
-  templateUrl: './project.html',
-  providers :[MyService],
+    selector: 'my-projects',
+    templateUrl: './project.html',
+    styleUrls: ['./basicTables.scss'],
+    providers: [MyService],
 })
 
 export class MyProjects {
 
- 
 
-    custom_search= false;
-    projctsStream : string = "projects";
-    projects : Project[] = []; 
 
-    constructor(private _router: Router ,private _service: MyService) {
+    custom_search = false;
+    projctsStream: string = "projects";
+    projects: Project[] = [];
+
+    constructor(private _router: Router, private _service: MyService) {
         _service.listStreamItems(this.projctsStream).then(data => {
             data.forEach(element => {
                 // console.log(this.Hex2String(element.data.toString()))
-                let project : Project = JSON.parse(this.Hex2String(element.data.toString()));
+                let project: Project = JSON.parse(this.Hex2String(element.data.toString()));
                 project.project_id = element.txid;
-                project.client =element.publishers[0];
+                project.client = element.publishers[0];
                 this.projects.push(project);
             });
             console.log(this.projects);
         });
     }
 
-    
 
-    Hex2String(hex_str:string) {
+
+    Hex2String(hex_str: string) {
         let j;
         let hexes = hex_str.match(/.{1,4}/g) || [];
         let result_back = "";
@@ -45,7 +46,7 @@ export class MyProjects {
         return result_back;
     }
 
-    goToProject(id : string){
+    goToProject(id: string) {
         let link = ['/project', id];
         this._router.navigate(link);
     }
