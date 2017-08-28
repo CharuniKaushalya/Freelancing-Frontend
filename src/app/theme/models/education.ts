e91afe6bd27db5b55da13cbb0a337e96a564daf3
Charuni Kaushalya <snkaushi@gmail.com>
2017-08-28 14:35:53 +0530

    add edu modal

---
+++ src/app/theme/models/education.ts
@@ -0,0 +1,15 @@
+/**
+ * Education
+ */
+
+ 
+export class Education {
+	public edu_id : string;
+    public username : string;
+	public country: string;
+    public university_name: string;
+    public title :string;
+	public degree: string;
+	public grad_year: number;
+    
+}
