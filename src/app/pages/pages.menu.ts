197aabad2c151054d4c2cf027a37899159e6f83e
Charuni Kaushalya <snkaushi@gmail.com>
2017-09-17 13:46:31 +0530

    bug fixing

--- src/app/pages/pages.menu.ts
+++ src/app/pages/pages.menu.ts
@@ -14,36 +14,6 @@ export const PAGES_MENU = [
             order: 0
           }
         }
-      },
-      {
-        path: 'contract',
-        data: {
-          menu: {
-            title: 'Contract',
-            icon: 'fa fa-balance-scale',
-            selected: false,
-            expanded: false,
-            order: 250,
-          }
-        },
-        children: [
-          {
-            path: 'mycontract',
-            data: {
-              menu: {
-                title: 'general.menu.mycontract',
-              }
-            }
-          },
-          {
-            path: 'contract_view',
-            data: {
-              menu: {
-                title: 'general.menu.contract_view',
-              }
-            }
-          }
-        ]
       }
     ]
   }
--- src/app/pages/privileges.service.ts
+++ src/app/pages/privileges.service.ts
@@ -20,6 +20,17 @@ export class privilegeService {
           title: 'general.menu.my_work'
         }
       ]
+    },
+    {
+      type: 'contract',
+      title: 'Contract',
+      icon: 'fa fa-balance-scale',
+      data: [
+        {
+          type: 'contract_view',
+          title: 'general.menu.contract_view'
+        }
+      ]
     }
   ],
   Consultant:[
@@ -37,6 +48,17 @@ export class privilegeService {
           title: 'general.menu.my_work'
         }
       ]
+    },
+     {
+      type: 'contract',
+      title: 'Contract',
+      icon: 'fa fa-balance-scale',
+      data: [
+        {
+          type: 'contract_view',
+          title: 'general.menu.contract_view'
+        }
+      ]
     }
   ],
   QA:[
@@ -52,6 +74,17 @@ export class privilegeService {
         {
           type: 'my_work',
           title: 'general.menu.my_work'
+        }
+      ]
+    },
+     {
+      type: 'contract',
+      title: 'Contract',
+      icon: 'fa fa-balance-scale',
+      data: [
+        {
+          type: 'contract_view',
+          title: 'general.menu.contract_view'
         }
       ]
     }
@@ -92,6 +125,21 @@ export class privilegeService {
         {
           type: 'my_work',
           title: 'general.menu.my_work'
+        }
+      ]
+    },
+    {
+      type: 'contract',
+      title: 'Contract',
+      icon: 'fa fa-balance-scale',
+      data: [
+        {
+          type: 'mycontract',
+          title: 'general.menu.mycontract'
+        },
+        {
+          type: 'contract_view',
+          title: 'general.menu.contract_view'
         }
       ]
     }
