daec5398b70320fe89119df50ce9c034cdf2434a
Charuni Kaushalya <snkaushi@gmail.com>
2017-09-22 18:40:09 +0530

    local user bid for specific project in project detail page

--- src/app/pages/work/components/post-projects/post_project.component.ts
+++ src/app/pages/work/components/post-projects/post_project.component.ts
@@ -23,6 +23,7 @@ export class PostedProjects implements OnInit {
     user_email:string;
 
     constructor(private _router: Router, private _service: MyService, private modalService: NgbModal, public authService: AuthService) {
+       console.log("init");
         this.authService.getAuth().authState.subscribe(user => {
             this.user_email = user.email;
         });
@@ -32,8 +33,7 @@ export class PostedProjects implements OnInit {
                 _service.gettxoutdata(element.txid).then(largedata => {
                     project = JSON.parse(this._service.Hex2String(largedata.toString()));
                     console.log("here come");
-                    if(project.user && project.user == this.user_email){
-                        console.log("here come 2");
+                    if(project.user && project.user == localStorage.getItem("user")){
                         project.project_id = element.txid;
                         project.client = element.publishers[0];
                         this.projects.push(project);
--- src/app/pages/work/components/project_detail/project_details.component.ts
+++ src/app/pages/work/components/project_detail/project_details.component.ts
@@ -2,6 +2,7 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { MyService } from "../../../../theme/services/backend/service";
 import { Project } from "../../../../theme/models/project";
 import { Bid } from "../../../../theme/models/bid";
+import { User } from "../../../../theme/models/user";
 
 import { TreeModel } from 'ng2-tree';
 import { Router, Params, ActivatedRoute } from '@angular/router';
@@ -18,7 +19,10 @@ export class ProjectDetails implements OnInit {
     bid: Bid;
     projctsStream: string = "projects";
     bidStream: string = "bid";
+    userstream: string = "Users";
     decodedFiles = [];
+    bids: Bid[] = [];
+    public bidcount: number = 0;
 
     constructor(private _service: MyService, private _route: ActivatedRoute, private _router: Router) {
         this._route.params.forEach((params: Params) => {
@@ -30,6 +34,33 @@ export class ProjectDetails implements OnInit {
                 });
 
                 let bid_key = project_id + "/" + localStorage.getItem("user");
+
+                _service.listStreamItems(this.bidStream).then(data => {
+                    data.forEach(element => {
+                        let bid_key = element.key;
+
+                        if (project_id == bid_key.split("/")[0]) {
+
+                            let bid: Bid;
+                            this.bidcount = 8;
+                            bid = JSON.parse(this._service.Hex2String(element.data.toString()));
+                            _service.listStreamKeyItems(this.userstream, bid.user_email.toString()).then(data => {
+                                let user: User;
+                                user = JSON.parse(this._service.Hex2String(data[0].data.toString()));
+                                console.log(user);
+                                bid.user_type = user.type;
+                                bid.user_name = user.name;
+                                bid.user_id  = data[0].txid;
+
+                            });
+                            this.bids.push(bid);
+                            
+                            
+                           
+                        }
+                    });
+
+                });
 
             } else {
 
@@ -47,5 +78,10 @@ export class ProjectDetails implements OnInit {
         document.body.appendChild(link);
         link.click();
         document.body.removeChild(link);
+    }
+
+    goToUser(id: string) {
+        let link = ['/pages/users/profile', id];
+        this._router.navigate(link);
     }
 }
